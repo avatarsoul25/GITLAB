f9e252ecc4977392e0dc1e2794b3414601efcc78
avatarsoul25 <avatarsoul25@gmail.com>
2015-04-26 00:55:15 +0700

    Test

--- Program#1.c
+++ Program#1.c
@@ -9,6 +9,8 @@ int main(){
 	printf("Enter size of the array: ");
 	scanf("%d", &size);
 	scanf("%d", &size);
+	scanf("%d", &size);
+	scanf("%d", &size);
 
 	printf("Enter %d elements: ", size);
 	for (i = 0; i<size; i++)
@@ -20,6 +22,8 @@ int main(){
 	printf("Sorted elements: ");
 	for (i = 0; i<size; i++)
 		printf(" %d", x[i]);
+	printf(" %d", x[i]);
+	printf(" %d", x[i]);
 	printf(" %d", x[i]);
 
 	return 0;
